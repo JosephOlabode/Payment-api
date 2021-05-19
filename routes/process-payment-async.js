42ac236b0e5a4bca6dc72f8d30bac9067f081dbe
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 10:49:59 +0000

    installed the uuid file

--- routes/process-payment-async.js
+++ routes/process-payment-async.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const processPayment = require('../models/process-payment');
+const uuid = require('uuid');
 
 
 router.post('/process-payment-async', async (req, res, next) => {
     const paymentDetails = req.body;
     if(paymentDetails.amount !== null && paymentDetails.clientId !== null && paymentDetails.narration !== null && paymentDetails.cardNumber !== null && paymentDetails.cvv !== null && paymentDetails.expiringDate !== null) {
         const paymentGateway = determinePaymentGateway(paymentDetails.amount);
+
+        const payload = new processPayment({
+
+        })
     }
     else {
         return res.status(422).send({
