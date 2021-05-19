4a95ba6bac7813a194bb14719265646337eddfbc
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 10:29:12 +0000

    implemented the process model payment

---
+++ models/process-payment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProcessPaymentSchema = new Schema({
+    amount: {type: Number, required: true},
+    clientId: {type: Number, required: true},
+    narration: {type: String, required: true},
+    cardNumber: {type: String, required: true},
+    cvv: {type: Number, required: true},
+    expiringDate: {type: Date, required: true}
+})
+
+module.exports = mongoose.model('process-payment', ProcessPaymentSchema, 'Payments');
