46fee7bc89f14700b72ff14da6eaaab735a8b24b
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 10:43:08 +0000

    implemented the determine payment gateway function

--- routes/process-payment-async.js
+++ routes/process-payment-async.js
@@ -14,3 +14,12 @@ router.post('/process-payment-async', async (req, res, next) => {
         })
     }
 })
+
+function determinePaymentGateway(amount) {
+    if(amount < 200000)
+        return 'Cheap Payment Gateway';
+    else if(amount >200000 && amount <= 500000)
+        return 'Expensive Payment Gateway';
+    else
+        return 'Premium Payment Gateway'
+}
