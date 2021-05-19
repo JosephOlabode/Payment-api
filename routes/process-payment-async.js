f3e2afe4da096df5c9d75494765f5833672f1030
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 10:37:02 +0000

    checking if all the details are supplied

--- routes/process-payment-async.js
+++ routes/process-payment-async.js
@@ -4,5 +4,13 @@ const processPayment = require('../models/process-payment');
 
 
 router.post('/process-payment-async', async (req, res, next) => {
+    const paymentDetails = req.body;
+    if(paymentDetails.amount !== null && paymentDetails.clientId !== null && paymentDetails.narration !== null && paymentDetails.cardNumber !== null && paymentDetails.cvv !== null && paymentDetails.expiringDate !== null) {
 
+    }
+    else {
+        return res.status(422).send({
+            message: 'Unprocessable request or incomplete data supplied'
+        })
+    }
 })
