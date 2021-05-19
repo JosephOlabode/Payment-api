09fa6a965135ce7bffc5fc09b1b45616b0d4c076
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 11:45:05 +0000

    implemented test for the helper function

--- test/unit/handler.test.js
+++ test/unit/handler.test.js
@@ -1,5 +1,10 @@
 const handler = require('../../controllers/helpers');
 
 describe('Determin Payment gateway', () => {
+    it('should use to amount to return the appropriate payment gateway', async() => {
+        const amount = 190000;
+        const result = await handler.determinePaymentGateway(amount);
 
+        expect(result).toBe('Cheap Payment Gateway');
+    })
 })
