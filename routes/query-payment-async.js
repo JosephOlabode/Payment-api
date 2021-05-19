c6ebb90362beb9fa9b0edf711e2153b63af4dd2e
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 11:20:14 +0000

    implemented the query payment router file

--- routes/query-payment-async.js
+++ routes/query-payment-async.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const queryPayment = require('../models/query-payment');
 
-router.get('query-payment/:paymentRef', (req, res, next) => {
+router.get('query-payment/:paymentRef', async(req, res, next) => {
     const paymentRef = req.params.paymentRef;
 
+    const payment = await queryPayment.findOne({paymentRef: paymentRef})
 
-
+    if(payment) {
+        return res.status(200).send(payment);
+    } else {
+        return res.status(404).send({
+            message: 'Payment not found'
+        })
+    }
 })
 module.exports = router
