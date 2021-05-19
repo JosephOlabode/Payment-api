49355c628dc70100e304c2ca8edbe38744a3de21
JosephOlabode <josepholabode17@gmail.com>
2021-05-19 10:31:37 +0000

    created the helpers controller file

--- routes/process-payment-async.js
+++ routes/process-payment-async.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const processPayment = require('../models/process-payment');
+
+
+router.post('/process-payment-async', async (req, res, next) => {
+
+})
